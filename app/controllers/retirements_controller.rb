5c657cfd72b1b26b5f2773065e3cc51f927f460a
Sam Wilks <sam.wilks92@gmail.com>
2014-06-16 00:09:25 -0400

    Currently working on a submit issue, will fix tomorrow

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class RetirementsController < ApplicationController
 	def new
 		@retirement = Retirement.new
@@ -6,11 +8,25 @@ class RetirementsController < ApplicationController
 	def create
 		@retirement = Retirement.new(params[:retirement])
 
-		@retirement.save
+		if @retirement.save
+			after_create
+		end 
+
+	end
+
+	def after_create
+		url = URI.parse('https://s3.amazonaws.com/fa-public/interview/retirement-data.json')
+		req = Net::HTTP::Get.new(url.path)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			http.request(req)
+		}
+		hash = JSON.parse res.body
+		@retirement.retirement_age = hash.fetch(@retirement.portfolio_type).fetch(@retirement.current_age).fetch(@retirement.current_amount).fetch(@retirement.anual_contributions).fetch(@retirement.desired_anual_income)
+		redirect_to @retirement
 	end
 
 	private
-		def article_params
-			params.requir(:retirement).permit(:portfolio_type, :current_age, :current_amount, :anual_contributions, :desired_anual_income)
+		def retirement_params
+			params.require(:retirement).permit(:portfolio_type, :current_age, :current_amount, :anual_contributions, :desired_anual_income)
 		end
 end
