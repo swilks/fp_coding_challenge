fb6a1db03400da112497d071acdfdd8ee63390e3
Sam Wilks <sam.wilks92@gmail.com>
2014-06-17 00:19:14 -0400

    Fully working app. No validation or styling, that is to come later

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -6,27 +6,22 @@ class RetirementsController < ApplicationController
 	end
 
 	def create
-		@retirement = Retirement.new(params[:retirement])
+		@retirement = Retirement.new(retirement_params)
 
 		if @retirement.save
-			after_create
-		end 
+			redirect_to retirements_path
+		else 
+			render 'new'
+		end
 
 	end
 
-	def after_create
-		url = URI.parse('https://s3.amazonaws.com/fa-public/interview/retirement-data.json')
-		req = Net::HTTP::Get.new(url.path)
-		res = Net::HTTP.start(url.host, url.port) {|http|
-			http.request(req)
-		}
-		hash = JSON.parse res.body
-		@retirement.retirement_age = hash.fetch(@retirement.portfolio_type).fetch(@retirement.current_age).fetch(@retirement.current_amount).fetch(@retirement.anual_contributions).fetch(@retirement.desired_anual_income)
-		redirect_to @retirement
+	def index
+		@retirements = Retirement.all
 	end
 
 	private
 		def retirement_params
-			params.require(:retirement).permit(:portfolio_type, :current_age, :current_amount, :anual_contributions, :desired_anual_income)
+			params.require(:retirement).permit(:portfolio_type, :current_age, :current_amount, :anual_contributions, :desired_anual_income, :retirement_age)
 		end
 end
--- app/models/retirement.rb
+++ app/models/retirement.rb
@@ -1,2 +1,11 @@
 class Retirement < ActiveRecord::Base
+	before_create :calculate_retirement_age;
+
+	private 
+		def calculate_retirement_age
+			file = File.read('lib/assets/retirement-data.json')
+			hash = JSON.parse(file)
+
+			self.retirement_age = hash.fetch(self.portfolio_type).fetch(self.current_age.to_s).fetch(self.current_amount.to_s).fetch(self.anual_contributions.to_s).fetch(self.desired_anual_income.to_s)
+		end
 end
